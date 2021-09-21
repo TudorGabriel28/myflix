1b491af39094075d6950fcd0ceeccc95413f56a6
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-09-21 14:01:19 +0300

    Add comparePasswords function to user model.

--- src/models/user.ts
+++ src/models/user.ts
@@ -19,64 +19,74 @@ interface UserDocument extends Document {
   role?: 'viewer' | 'admin' | 'superUser';
   createdAt: Date;
   updatedAt: Date;
+  // eslint-disable-next-line no-unused-vars
+  comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
 const userSchema = new Schema(
   {
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate: {
-      validator(v: string) {
-        return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-          v
-        );
-      },
-      message: 'Please enter a valid email address'
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate: {
+        validator(v: string) {
+          return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          );
+        },
+        message: 'Please enter a valid email address'
+      }
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator(v: string) {
+          return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%_*?&]).{8,}$/.test(
+            v
+          );
+        },
+        message:
+          'Your password must have one lowercase, one uppercase, one number and one special character'
+      }
+    },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    nickname: {
+      type: String,
+      trim: true
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+    role: {
+      type: String,
+      default: 'viewer'
     }
-  },
-  password: {
-    type: String,
-    required: true,
-    validate: {
-      validator(v: string) {
-        return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%_*?&]).{8,}$/.test(
-          v
-        );
-      },
-      message:
-        'Your password must have one lowercase, one uppercase, one number and one special character'
-    }
-  },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  nickname: {
-    type: String,
-    trim: true
-  },
-  active: {
-    type: Boolean,
-    default: false
-  },
-  role: {
-    type: String,
-    default: 'viewer'
-  }
-  // friends: [{ type: Schema.Types.ObjectId, ref: 'Friends' }]
+    // friends: [{ type: Schema.Types.ObjectId, ref: 'Friends' }]
   },
   { timestamps: true }
 );
+
+userSchema.methods.comparePassword = async function comparePassword(
+  candidatePassword: string
+) {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch(() => false);
+};
 
 userSchema.pre('save', async function hashPassword(next) {
   const user = this as UserDocument;
