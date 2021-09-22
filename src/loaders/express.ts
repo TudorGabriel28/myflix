3f79a1db6c4a23a8b824939e1437a0447547a7c9
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-09-23 01:11:10 +0300

    Create session router.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import userRouter from '../routes/user';
+import sessionRouter from '../routes/session';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/users', userRouter);
+app.use('/api/users', userRouter);
+app.use('/api/sessions', sessionRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
