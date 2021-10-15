ccc9f266da1183a6d527b8ed94d93447295cfffe
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-10-15 13:52:58 +0300

    Create get all users route.

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import createUserHandler from '../controllers/user';
+import { createUserHandler, getAllUsersHandler } from '../controllers/user';
 
 const userRouter = Router();
 
 // Register user
 userRouter.post('/', createUserHandler);
+
+// Get all users
+userRouter.get('/', getAllUsersHandler);
 
 export default userRouter;
