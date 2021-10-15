1249ee91c8652a31009963252a27309d36c6fc8e
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-10-15 21:29:54 +0300

    Add get user sessions route.

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import {
   createUserSessionHandler,
-  invalidateUserSessionHandler
+  invalidateUserSessionHandler,
+  getUserSessionsHandler
 } from '../controllers/session';
 
 const sessionRouter = Router();
@@ -11,5 +12,8 @@ sessionRouter.post('/', createUserSessionHandler);
 
 // Logout user
 sessionRouter.delete('/', invalidateUserSessionHandler);
+
+// Get the user's sessions
+sessionRouter.get('/', getUserSessionsHandler);
 
 export default sessionRouter;
