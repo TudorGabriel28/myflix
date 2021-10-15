71e9881476b487e92a1d027d16543f13496aac39
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-10-15 15:28:22 +0300

    Create getUserHandler() in controller.

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createUser, getAllUsers } from '../services/user';
+import { createUser, getAllUsers, findUser } from '../services/user';
 import { sendAccountActivationMail } from '../services/mailer';
 
 export async function createUserHandler(req: Request, res: Response) {
@@ -8,7 +8,6 @@ export async function createUserHandler(req: Request, res: Response) {
     await sendAccountActivationMail(user);
     return res.sendStatus(201);
   } catch (error: any) {
-    console.log(error);
     return res.status(409).send(error.message);
   }
 }
@@ -18,7 +17,15 @@ export async function getAllUsersHandler(req: Request, res: Response) {
     const users = await getAllUsers();
     return res.status(200).send(users);
   } catch (error: any) {
-    console.log(error);
+    return res.status(400).send(error.message);
+  }
+}
+
+export async function getUserHandler(req: Request, res: Response) {
+  try {
+    const user = await findUser({ _id: req.params.userId });
+    return res.status(200).send(omit(user, 'password'));
+  } catch (error: any) {
     return res.status(400).send(error.message);
   }
 }
