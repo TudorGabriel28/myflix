e2460d5331e70a73fea506ed1db620a07803a213
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-10-16 20:31:32 +0300

    Updated parameters for mailer service functions.

--- src/services/mailer.ts
+++ src/services/mailer.ts
@@ -1,22 +1,22 @@
-import { FilterQuery } from 'mongoose';
 import { UserDocument } from '../models/user'
 import mailer from '../utils/mailer'
 
-export async function sendAccountActivationMail(email: FilterQuery<UserDocument>, id: FilterQuery<UserDocument>) {
+export async function sendAccountActivationMail(user: UserDocument) {
   const mailContent = {
     from: process.env.MAILER_USER,
-    to: email,
+    to: user.email,
     subject: 'Account activation',
-    text: `http://localhost:3000/users/activation/${id}`
+    // eslint-disable-next-line no-underscore-dangle
+    text: `http://localhost:3000/users/activation/${user._id}`
   };
 
   await mailer(mailContent);
 }
 
-export async function sendResetPasswordMail(email: FilterQuery<UserDocument>, tokenId: FilterQuery<UserDocument>) {
+export async function sendResetPasswordMail(user: UserDocument, tokenId: string) {
   const mailContent = {
     from: process.env.MAILER_USER,
-    to: email,
+    to: user.email,
     subject: 'Reset password',
     text: `http://localhost:3000/users/reset-password/${tokenId}`
   };
