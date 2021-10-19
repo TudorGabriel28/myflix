04dfd290118365b5dd238543333d0b489503e6b5
Tudor Gabriel <tudordangabriel28@gmail.com>
2021-10-19 16:23:17 +0300

    Add author and platform properties to movie model.

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -2,13 +2,17 @@ import mongoose from 'mongoose';
 
 export interface MovieDocument extends mongoose.Document {
   title: String;
+  author: String;
+  platform?: String;
   createdAt: Date;
   updatedAt: Date;
 }
 
 const MovieSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true }
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    platform: { type: String }
   },
   { timestamps: true }
 );
